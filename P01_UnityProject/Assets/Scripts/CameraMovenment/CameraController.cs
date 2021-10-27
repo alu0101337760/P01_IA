f547b89bf23718a2b14223b722da4b73e1ac5292
Tana <tanausufc@gmail.com>
2021-10-27 18:13:22 +0100

    Camera script completed

---
+++ P01_UnityProject/Assets/Scripts/CameraMovenment/CameraController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace IA_sim 
+{
+    public class CameraController : MonoBehaviour
+    {
+        public float panSpeed = 20f;
+        public float panBorderThickness = 10f;
+        public float scrollSpeed = 40f;
+        public float minY = 10f;
+        public float maxY = 120f;
+
+        void Update()
+        {
+            Vector3 pos = transform.position;
+
+            if (Input.GetKey("w") || Input.mousePosition.y >= (Screen.height - panBorderThickness))
+            {
+                pos.z += panSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+            {
+                pos.z -= panSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey("d") || Input.mousePosition.x >= (Screen.width - panBorderThickness))
+            {
+                pos.x += panSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+            {
+                pos.x -= panSpeed * Time.deltaTime;
+            }
+
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            pos.y -= scroll * scrollSpeed * 100 * Time.deltaTime;
+
+            pos.x = Mathf.Clamp(pos.x, -PlacementManager.instance.maxX - 5, PlacementManager.instance.maxX + 5);
+            pos.y = Mathf.Clamp(pos.y, minY, float.MaxValue);
+            pos.z = Mathf.Clamp(pos.z, -PlacementManager.instance.maxZ - 5, PlacementManager.instance.maxZ + 5);
+
+            transform.position = pos;
+        }
+    }
+}
+
+
